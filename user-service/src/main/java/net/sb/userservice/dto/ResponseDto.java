766a4d867843c683abed656966e764dcfff57d9b
alpeshjadhav <djalpesh786@gmail.com>
2024-06-17 20:58:48 +0530

    Spring Boot using WebClient

--- user-service/src/main/java/net/sb/userservice/dto/ResponseDto.java
+++ user-service/src/main/java/net/sb/userservice/dto/ResponseDto.java
@@ -13,20 +13,4 @@ public class ResponseDto {
 
     private DepartmentDto department;
     private UserDto user;
-
-    public DepartmentDto getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(DepartmentDto department) {
-        this.department = department;
-    }
-
-    public UserDto getUser() {
-        return user;
-    }
-
-    public void setUser(UserDto user) {
-        this.user = user;
-    }
 }
--- user-service/src/main/java/net/sb/userservice/entity/User.java
+++ user-service/src/main/java/net/sb/userservice/entity/User.java
@@ -17,53 +17,9 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String firstName;
-
     private String lastName;
-
     @Column(nullable = false, unique = true)
     private String email;
-
     private String departmentId;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getDepartmentId() {
-        return departmentId;
-    }
-
-    public void setDepartmentId(String departmentId) {
-        this.departmentId = departmentId;
-    }
 }
